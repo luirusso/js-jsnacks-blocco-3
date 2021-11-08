97c3b5c96f3491dcf7a392a709ca41a89d616e59
luirusso <luirusso_@hotmail.it>
2021-11-08 15:02:40 +0100

    completed second snack

--- snack2/index.js
+++ snack2/index.js
@@ -4,3 +4,13 @@ A partire da un array di numeri, genera un secondo array con le potenze al quadr
 Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
  */
 
+const numbers = [1, 2, 3, 4, 5];
+console.log('Numeri originali:', numbers);
+
+const squaredNumbers = numbers.map((number) => {
+    return number * number;
+});
+
+console.log('Numeri al quadrato', squaredNumbers);
+
+console.log('Numeri originali dopo la creazione del secondo array', numbers);
